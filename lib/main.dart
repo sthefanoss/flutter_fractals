43654c6b5d150e81ec6bb3a49a49aa56bff1d733
sthefano <sthefanoss@gmail.com>
2022-08-01 22:45:09 -0300

    test with shaders

--- lib/main.dart
+++ lib/main.dart
@@ -1,210 +1,98 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'dart:math';
-
-int maxCount = 1048;
-
-int getM(Point<double> c, Point<double> z0) {
-  int count = 0;
-  double xk = z0.x;
-  double yk = z0.y;
-  double xk1;
-  double yk1;
-  while (count < maxCount) {
-    xk1 = xk * xk - yk * yk + c.x;
-    yk1 = 2 * xk * yk + c.y;
-    xk = xk1;
-    yk = yk1;
-    count++;
-    if (xk * xk + yk * yk >= 10E5) {
-      return count;
-    }
-  }
-  return -1;
-}
+import 'package:flutter/services.dart';
+import 'foo_sprv.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: Page()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
+/// Will combine loading multiple things
+class PainterNeeds {
+  final ImageShader imageShader;
+  final FragmentProgram fragmentProgram;
+
+  PainterNeeds(this.imageShader, this.fragmentProgram);
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+/// Loads JPEG image and the [FragmentProgram]
+Future<PainterNeeds> loadPainterNeeds() async {
+  final asset = await rootBundle.load("assets/image.png");
+  final image = await decodeImageFromList(asset.buffer.asUint8List());
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  /// Create ImageShader that will provide a GLSL sampler
+  final ImageShader imageShader = ImageShader(
+    image,
+    // Specify how image repetition is handled for x and y dimension
+    TileMode.repeated,
+    TileMode.repeated,
+    // Transformation matrix (identity matrix = no transformation)
+    Matrix4.identity().storage,
+  );
+
+  return PainterNeeds(imageShader, await fooFragmentProgram());
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  late List<Color> colors;
-  double fWidthBase = 4;
-  double fHeightBase = 4;
-  double fMagnitude = 1;
-  double get fWidth => fWidthBase * exp(fMagnitude);
-  double get fHeight => fHeightBase * exp(fMagnitude);
-  Offset fCenter = Offset.zero;
-  Offset panOffset = Offset.zero;
-  Offset? px, dpx;
-  Offset? sumPx;
-  int xSteps = 100;
-  int ySteps = 100;
-
-  double get xMax => fCenter.dx + panOffset.dx + fWidth / 2;
-  double get xMin => fCenter.dx + panOffset.dx - fWidth / 2;
-  double get yMax => fCenter.dy + panOffset.dy + fHeight / 2;
-  double get yMin => fCenter.dy + panOffset.dy - fHeight / 2;
-
-  Color colorByInt(int x) {
-    if (x == -1) {
-      return Colors.black;
-    }
-    return colors[x];
-  }
-
-  List<Color> lerpGenerator(int outputLength, List<Color> input) {
-    final colors = <Color>[];
-    double ratio = outputLength / (input.length - 1);
-    for (int i = 0; i < outputLength; i++) {
-      int colorIndex = i ~/ ratio;
-      double v = i / ratio - colorIndex;
-      colors.add(
-        ColorTween(begin: input[colorIndex], end: input[colorIndex + 1])
-            .lerp(v)!,
-      );
-    }
-    return colors..add(input.last);
-  }
-
-  @override
-  void initState() {
-    colors = lerpGenerator(maxCount, [
-      Colors.deepPurple,
-      Colors.indigo,
-      Colors.blueAccent,
-      Colors.green,
-      Colors.yellowAccent,
-      Colors.orange,
-      Colors.red,
-      Colors.purple
-    ]);
-    super.initState();
-  }
+class Page extends StatelessWidget {
+  const Page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final image = <Alignment, Color>{};
-    double dAx = 2 / (xSteps - 1);
-    double dAy = 2 / (ySteps - 1);
-    double dx = fWidth / (xSteps - 1);
-    double dy = fHeight / (ySteps - 1);
-    for (int i = 0; i < xSteps; i++) {
-      final x = xMin + dx * i;
-      final ax = -1 + dAx * i;
-      for (int j = 0; j < ySteps; j++) {
-        final y = yMin + dy * j;
-        final ay = -1 + dAy * j;
-        image[Alignment(ax, ay)] =
-            colorByInt(getM(Point(x, y), const Point<double>(0, 0)));
-      }
-    }
-
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            const Spacer(),
-            LayoutBuilder(
-              builder: (context, constraints) {
-                final height = constraints.maxWidth / ySteps;
-                final width = constraints.maxWidth / xSteps;
+      body: FutureBuilder<PainterNeeds>(
 
-                return Center(
-                  child: SizedBox(
-                    height: constraints.maxWidth,
-                    width: constraints.maxWidth,
-                    child: GestureDetector(
-                      onPanStart: (value) {
-                        px = value.localPosition;
-                        panOffset = Offset.zero;
-                      },
-                      onPanUpdate: (value) {
-                        dpx = (value.localPosition - px!);
+        /// Use the generated loader function here
+          future: loadPainterNeeds(),
+          builder: ((context, snapshot) {
+            if (!snapshot.hasData) {
+              /// Shader is loading
+              return const CircularProgressIndicator();
+            }
 
-                        final sdx = -dpx!.dx * fWidth / constraints.maxWidth;
-                        final sdy = -dpx!.dy * fHeight / constraints.maxWidth;
-
-                        setState(() => panOffset = Offset(sdx, sdy));
-                      },
-                      onPanEnd: (_) {
-                        setState(() {
-                          fCenter += panOffset;
-                          panOffset = Offset.zero;
-                        });
-                      },
-                      child: Stack(children: [
-                        ...image.entries.map<Widget>((entry) {
-                          return Align(
-                            alignment: entry.key,
-                            child: Container(
-                              height: height,
-                              width: width,
-                              decoration: BoxDecoration(color: entry.value),
-                            ),
-                          );
-                        }).toList(),
-                      ]),
-                    ),
-                  ),
-                );
-              },
-            ),
-            const Spacer(),
-            SizedBox(
-              height: 50,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: colors.length,
-                itemBuilder: (c, i) => Card(
-                  child: SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Text(i.toString()),
-                  ),
-                  color: colors[i],
-                ),
+            /// Shader is ready to use
+            return SizedBox.expand(
+              child: CustomPaint(
+                painter: ImageScaleShaderPainter(snapshot.data!),
               ),
-            ),
-            Text(
-              'order: 10^${fMagnitude.toStringAsFixed(2)}\n'
-              'center: $fCenter\n'
-              '${xMin.toStringAsExponential(2)}<=x<=${xMax.toStringAsExponential(2)}, width = ${fWidth.toStringAsExponential(2)}\n'
-              '${yMin.toStringAsExponential(2)}<=y<=${yMax.toStringAsExponential(2)}, height = ${fHeight.toStringAsExponential(2)}',
-            ),
-            Slider(
-              value: fMagnitude,
-              min: -34,
-              max: 1,
-              onChanged: (v) {
-                setState(() {
-                  fMagnitude = v;
-                });
-              },
-            ),
-          ],
-        ),
-      ),
+            );
+          })),
     );
+  }
+}
+
+/// Customer painter that makes use of the shader
+class ImageScaleShaderPainter extends CustomPainter {
+  ImageScaleShaderPainter(this.painterNeeds);
+
+  final PainterNeeds painterNeeds;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    /// Create paint using a shader
+    final paint = Paint()
+      ..shader = painterNeeds.fragmentProgram.shader(
+        floatUniforms: Float32List.fromList([
+          // scale uniform
+          0.5,
+        ]),
+        samplerUniforms: [
+          painterNeeds.imageShader,
+        ],
+      );
+
+    /// Draw a rectangle with the shader-paint
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    if (oldDelegate is ImageScaleShaderPainter &&
+        oldDelegate.painterNeeds == painterNeeds) {
+      /// Do not repaint when painter has same set of properties
+      return false;
+    }
+    return true;
   }
 }
