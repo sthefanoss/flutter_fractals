7d5e0342a8fafe3d237974de96bd4ceb4c4c78db
sthefano <sthefanoss@gmail.com>
2022-08-05 01:10:21 -0300

    use old controllers for shader mandelbrot

--- lib/foo.glsl
+++ lib/foo.glsl
@@ -6,17 +6,19 @@ layout(location=0) out vec4 fragColor;
 
 layout(location=0) uniform float scale;
 layout(location=1) uniform vec2 center;
+layout(location=2) uniform vec2 iResolution;
 
 void main(){
+    vec2 uv = gl_FragCoord.xy/iResolution.xy;
     vec2 z = vec2(0.0);
     vec2 c = vec2(1.0);
     float ii = 0.0;
-    c.x = 1.3333 * (gl_FragCoord.x - 0.5) * scale - center.x;
-    c.y = (gl_FragCoord.y - 0.5) * scale - center.y;
-    for (float i=0.0;i<0.99;i+=0.01) {
+    c.x = 1.3333 * (uv.x - 0.5) * scale - center.x;
+    c.y = (uv.y - 0.5) * scale - center.y;
+    for (float i=0.0;i<0.99;i+=0.001) {
         float x = (z.x * z.x - z.y * z.y) + c.x;
         float y = (z.y * z.x + z.x * z.y) + c.y;
-        ii = i;
+        ii = 1.0- i;
         if((x * x + y * y) > 4.0) break;
         z.x = x;
         z.y = y;
--- lib/main.dart
+++ lib/main.dart
@@ -2,59 +2,150 @@ import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'foo_sprv.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MaterialApp(home: Page()));
+  runApp(const MyApp());
 }
 
-class Page extends StatefulWidget {
-  const Page({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<Page> createState() => _PageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _PageState extends State<Page> {
+class _MyHomePageState extends State<MyHomePage> {
+  double fMagnitude = 1;
+  double get scale => exp(fMagnitude);
+  Offset fCenter = Offset(0.5, 0);
+  Offset panOffset = Offset.zero;
+  Offset? px, dpx;
+  Offset? sumPx;
+
+  double get max => fCenter.dx + panOffset.dx + scale / 2;
+  double get min => fCenter.dx + panOffset.dx - scale / 2;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<FragmentProgram>(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Spacer(),
+            LayoutBuilder(
+              builder: (context, constraints) {
+                return Center(
+                  child: SizedBox(
+                    height: constraints.maxWidth,
+                    width: constraints.maxWidth,
+                    child: GestureDetector(
+                      onPanStart: (value) {
+                        px = value.localPosition;
+                        panOffset = Offset.zero;
+                      },
+                      onPanUpdate: (value) {
+                        dpx = (value.localPosition - px!);
 
-          /// Use the generated loader function here
-          future: fooFragmentProgram(),
-          builder: ((context, snapshot) {
-            print(snapshot.error);
-            if (!snapshot.hasData) {
-              /// Shader is loading
-              return const CircularProgressIndicator();
-            }
+                        final sdx = dpx!.dx * scale / constraints.maxWidth;
+                        final sdy = dpx!.dy * scale / constraints.maxWidth;
 
-            /// Shader is ready to use
-            return SizedBox.expand(
-              child: CustomPaint(
-                painter: ImageScaleShaderPainter(snapshot.data!),
-              ),
-            );
-          })),
+                        setState(() => panOffset = Offset(sdx, sdy));
+                      },
+                      onPanEnd: (_) {
+                        setState(() {
+                          fCenter += panOffset;
+                          panOffset = Offset.zero;
+                        });
+                      },
+                      child: FutureBuilder<FragmentProgram>(
+
+                          /// Use the generated loader function here
+                          future: fooFragmentProgram(),
+                          builder: ((context, snapshot) {
+                            print(snapshot.error);
+                            if (!snapshot.hasData) {
+                              /// Shader is loading
+                              return const CircularProgressIndicator();
+                            }
+
+                            /// Shader is ready to use
+                            return SizedBox.expand(
+                              child: CustomPaint(
+                                painter: ImageScaleShaderPainter(
+                                  painterNeeds: snapshot.data!,
+                                  center: fCenter + panOffset,
+                                  scale: scale,
+                                ),
+                              ),
+                            );
+                          })),
+                    ),
+                  ),
+                );
+              },
+            ),
+            const Spacer(),
+            Text('order: 10^${fMagnitude.toStringAsFixed(2)}\n'
+                'center: ${fCenter + panOffset}\n'
+                '${min.toStringAsExponential(2)}<=x<=${max.toStringAsExponential(2)}, width = ${scale.toStringAsExponential(2)}\n'
+                //'${yMin.toStringAsExponential(2)}<=y<=${yMax.toStringAsExponential(2)}, height = ${fHeight.toStringAsExponential(2)}',
+                ),
+            Slider(
+              value: fMagnitude,
+              min: -34,
+              max: 1,
+              onChanged: (v) {
+                setState(() {
+                  fMagnitude = v;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 /// Customer painter that makes use of the shader
 class ImageScaleShaderPainter extends CustomPainter {
-  ImageScaleShaderPainter(this.painterNeeds);
+  const ImageScaleShaderPainter({
+    required this.painterNeeds,
+    required this.center,
+    required this.scale,
+  });
+
+  final Offset center;
+  final double scale;
   final FragmentProgram painterNeeds;
+
   @override
   void paint(Canvas canvas, Size size) {
     /// Create paint using a shader
     final paint = Paint()
       ..shader = painterNeeds.shader(
         floatUniforms: Float32List.fromList([
-          0.001,
-          1.0,
-          1.0,
+          scale,
+          center.dx,
+          center.dy,
+          size.width,
+          size.height,
         ]),
       );
 
@@ -64,11 +155,6 @@ class ImageScaleShaderPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    if (oldDelegate is ImageScaleShaderPainter &&
-        oldDelegate.painterNeeds == painterNeeds) {
-      /// Do not repaint when painter has same set of properties
-      return false;
-    }
     return true;
   }
 }
